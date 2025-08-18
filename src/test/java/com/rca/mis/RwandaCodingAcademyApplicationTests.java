45f72fe12a7a679dfc8cdb948c18713d1db2797c
IRASUBIZA-SALY-NELSON <irasubizasalynelson@gmail.com>
2025-08-18 19:33:26 +0100

    deployment2

--- src/test/java/com/rca/mis/RwandaCodingAcademyApplicationTests.java
+++ src/test/java/com/rca/mis/RwandaCodingAcademyApplicationTests.java
@@ -2,8 +2,24 @@ package com.rca.mis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.flyway.enabled=false",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration",
+        "app.jwt.secret=test-jwt-secret-key-for-testing-only-make-it-long-enough",
+        "app.jwt.expiration=86400000",
+        "app.jwt.refresh-expiration=604800000"
+    }
+)
+@ActiveProfiles("test")
 class RwandaCodingAcademyApplicationTests {
 
 	@Test
